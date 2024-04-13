71d5a4647c9fa5789c81c4b0955d62184eb90fa8
Manumilde <101014747+Manumilde@users.noreply.github.com>
2024-04-13 12:45:38 -0300

    Update main.cpp

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -2,6 +2,35 @@
 #include <iostream>
 #include <fstream>
 #include "include/json.hpp"
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+double f_en_tramo(double x0, double y0, double x1, double y1, double x){
+    
+    double a = (((y1-y0)/(x1-x0))*(x-x0)) + y0;
+
+    return a;
+};
+
+double estimar_error_y(sol,x,y){
+
+    int i = 0;
+    double error = 0;
+
+    while (i < (sol.size()-1)){
+
+        vector<double> sub_x, sub_y;
+        tie(sub_x, sub_y) = subconjunto(x, y, sol[i].first, sol[i+1].first);
+        vector<double> prediccion = f_en_tramo(sol[i].first, sol[i].second, sol[i+1].first, sol[i+1].second, sub_x);
+        error = error + calcular_error(prediccion, sub_y);
+        i = i + 1;
+    }
+
+    return error;
+    
+};
 
 // Para libreria de JSON.
 using namespace nlohmann;
